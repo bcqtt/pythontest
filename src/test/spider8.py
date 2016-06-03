1ff9a1736eb214a7324e7bb6b70b3adc811c3571
Jayone Lai <lz881228@hotmail.com>
2016-06-03 18:06:22 +0800

    更新
    练习HTMLParser

--- src/test/spider8.py
+++ src/test/spider8.py
@@ -5,7 +5,33 @@ Created on 2016年5月31日
 '''
 import http.cookiejar
 import urllib
-from test import spider7
+import gzip
+import re
+from xml import etree
+import sys
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    a_text = False
+    
+    def handle_starttag(self, tag, attrs):
+        #print("开始一个标签:",tag)
+        print()
+        if str(tag).startswith("title"):
+            print(tag)
+            self.a_t=True
+            for attr in attrs:
+                print("属性值：",attr)
+    
+    def handle_data(self,data):  
+        if self.a_text:  
+            print (data)
+            
+    def handle_endtag(self, tag):
+        if tag == "h1":
+            self.a_t=False
+            #print("结束一个标签:",tag)
+
 header = {
     'Connection': 'Keep-Alive',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -25,11 +51,65 @@ def getOpener(head):
         header.append(elem)
     opener.addheaders = header
     return opener
-         
 
-url = "http://lz881228.blog.163.com/blog/#m=0"
-opener = getOpener(header)
-op = opener.open(url)
-data = op.read()
-data = spider7.ungzip(data)
-print(data.decode())
+def ungzip(data):
+    try:
+        print("尝试解压缩...")
+        data = gzip.decompress(data)
+        print("解压完毕")
+    except:
+        print("未经压缩，无需解压")
+    
+    return data
+
+def getPageUrls(data):
+    pattern = re.compile('\"http://www.williamlong.info/archives/(\d*).html\" rel=\"bookmark\"')
+    strlist = pattern.findall(data)
+    return strlist
+
+def getPageNumUrls(data):
+    pattern = re.compile('http://www.williamlong.info/cat/\?page=(\d*)')
+    urllist = pattern.findall(data)
+    return urllist
+'''
+def getArticleTitle(data):
+    #pattern = re.compile('http://www.williamlong.info/cat/\?page=(\d*)')
+    #urllist = pattern.findall(data)
+    try:
+        tree = etree.ElementTree(data)
+        nodes = tree.xpath("//h1[@class='post-title']")  
+    except:
+        sys.exit()
+    return nodes
+'''
+
+def getData(article_url):
+    opener = getOpener(header)
+    op = opener.open(url)
+    data = op.read()
+    data = ungzip(data)
+    return data
+
+url = "http://www.williamlong.info/"
+data = getData(url);
+
+str_html = data.decode("UTF-8")
+article_urls = getPageUrls(str_html)
+page_num_urls = getPageNumUrls(str_html)
+
+print("抓到的URL数：%d" % len(article_urls) )
+
+for a in article_urls:
+    article_url = "http://www.williamlong.info/archives/%s.html" % a
+    print("爬取文章链接：%s" % article_url)
+    articlePage = getData(article_url).decode("UTF-8")
+    print(articlePage)
+    my = MyHTMLParser()
+    my.feed(articlePage)
+    
+print("抓到的页码URL数：%d" % len(page_num_urls) )
+
+for p in page_num_urls:
+    print("http://www.williamlong.info/cat/?page=%s" % p)
+
+#print(data.decode('UTF-8'))
---
+++ src/test/spider9.py
@@ -0,0 +1,5 @@
+'''
+Created on 2016年6月3日
+
+@author: gionee
+'''
