ba10c96d117d74a509be92997ed382fe65678114
Jayone Lai <lz881228@hotmail.com>
2016-09-13 15:49:11 +0800

    更新
    修改爬虫Header

--- src/novel/novel_spider.py
+++ src/novel/novel_spider.py
@@ -6,38 +6,56 @@ Created on 2016年9月12日
 import urllib
 from bs4 import BeautifulSoup
 import re
+import http
 
-def grabing(url):
-    
+def confOper(head={
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'zip, deflate, sdch',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',                                  
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    }):
+    cj = http.cookiejar.CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    header = []
+    for key, value in head.items():
+        elem = (key, value)
+        header.append(elem)
+    opener.addheaders = header
+    return opener
+
+def getReq(url):
     req = urllib.request.Request(url,headers={
-        'Connection': 'Keep-Alive',
+        'Connection': 'keep-alive',
         'Accept-Encoding': 'zip, deflate, sdch',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',                                  
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
     })
+    return req
+
+def grabing(url):
     
-    oper = urllib.request.urlopen(req)
+    oper = urllib.request.urlopen(getReq(url))
     data = oper.read()
     
     soup = BeautifulSoup(data)
     a_list = soup.find_all("a",attrs={"itemprop": "url"})
     a_list.pop(0)
-    url_list = []
     for atag in a_list:
         url = re.search(r'http://read.qidian.com/BookReader/4fknnsotQvLZ6ZDT--NUMw2,?.*\.aspx',str(atag))
         txt = re.search(r'<span itemprop="headline">.*</span>',str(atag))
         soup = BeautifulSoup(txt.group())
         print(soup.getText())
         
-        oper = urllib.request.urlopen(url.group())
+        oper = urllib.request.urlopen(getReq(url.group()))
         data = oper.read()
         soup = BeautifulSoup(data)
         
         div_content = soup.find("div",id="chaptercontent")
         txt_url = re.search(r'http://files.qidian.com/Author6/?.*\.txt', str(div_content),re.DOTALL)
         
-        oper = urllib.request.urlopen(txt_url)
+        oper = urllib.request.urlopen(getReq(txt_url))
         data = oper.read()
         content = str(data).decode("UTF-8")
         print(content)
